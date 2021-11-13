db82b7faf132b9d11b6c7f99dfde44bc9ac2ed4f
pink-balloons <ambergray930@yahoo.com>
2021-11-13 10:48:33 -0500

    added css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import "./Home.css";
@@ -12,17 +11,3 @@ const Home = () => {
 };
 
 export default Home;
-=======
-import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
-
-const Home = () => {
-  return (
-    <header>
-      <h1>Welcome to Home Component</h1>
-    </header>
-  )
-}
-
-export default Home;
->>>>>>> 75b4903a09c2e0ca0ff0779e6700acfbbc52a28a
